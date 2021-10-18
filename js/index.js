e6c5a2dd56beba64c1f8fbd0f7389f874f33578c
Maik Jüngling <juengling.maik@gmail.com>
2021-10-18 18:04:04 +0200

    changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 // Iteration 1: Names and Input
-let hacker1 = "Manish";
-console.log("The driver's name is " + hacker1 );
+let hacker1 = `Manish`;
+console.log(`The driver's name is ${hacker1}.`);
 
+let hacker2 = `Manic Manish`;
+console.log(`The navigator's name is ${hacker2}.`); 
 
 // Iteration 2: Conditionals
 
